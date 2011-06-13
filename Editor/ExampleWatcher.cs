c6f2f1bd9a3603044ece72366172e1720a28e865
Michael Stevenson <michael@mstevenson.net>
2011-06-13 00:52:50 -0700

    Created example code stub

---
+++ Editor/ExampleWatcher.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+
+[ExecuteInEditMode]
+public static class ExampleWatcher
+{
+	
+	/// <summary>
+	/// Static constructor called immediately when the project loads.
+	/// </summary>
+	static ExampleWatcher ()
+	{
+//		AssetsWatcher watcher = new AssetsWatcher ("", UnityAssetType.Material);
+	}
+	
+}
