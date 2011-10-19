0071282fabca9e4a5d16a01b369fb1376a685838
Michael Stevenson <michael@mstevenson.net>
2011-10-18 23:56:11 -0700

    Completed an AssetsWatcher usage example

--- Editor/ExampleWatcher.cs
+++ Editor/ExampleWatcher.cs
@@ -1,15 +1,40 @@
 using UnityEditor;
+using UnityEngine;
 
-[ExecuteInEditMode]
+// Super secret undocumented attribute that calls a static constructor when the Unity editor loads.
+[InitializeOnLoad]
+/// <summary>
+/// Example AssetsWater usage.
+/// </summary>
 public static class ExampleWatcher
-{
-	
+{	
 	/// <summary>
 	/// Static constructor called immediately when the project loads.
 	/// </summary>
 	static ExampleWatcher ()
 	{
-//		AssetsWatcher watcher = new AssetsWatcher ("", UnityAssetType.Material);
+		AssetsWatcher watcher = new AssetsWatcher ("", UnityAssetType.Material);
+		Debug.Log ("Began watching '" + watcher.Path + "' for asset changes");
+		
+		watcher.OnCreated += delegate(AssetFileInfo asset) {
+			Debug.Log ("Created asset '" + asset.Name + "' of type " + asset.Type);
+		};
+		
+		watcher.OnDeleted += delegate(AssetFileInfo asset) {
+			Debug.Log ("Deleted asset '" + asset.Name + "' of type " + asset.Type);
+		};
+		
+		watcher.OnModified += delegate(AssetFileInfo asset) {
+			Debug.Log ("Changed asset '" + asset.Name + "' of type " + asset.Type);
+		};
+		
+		watcher.OnMoved += delegate(AssetFileInfo assetBefore, AssetFileInfo assetAfter) {
+			Debug.Log ("Moved asset '" + assetBefore.Name + "' to '" + assetAfter.Name + "'");
+		};
+		
+		watcher.OnRenamed += delegate(AssetFileInfo assetBefore, AssetFileInfo assetAfter) {
+			Debug.Log ("Renamed asset '" + assetBefore.Name + "' to '" + assetAfter.Name + "'");
+		};
 	}
 	
 }
